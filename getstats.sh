19acf48e72c382d1fde55b81087204ab96172ac1
suresh666kumar <suresh_kumar3@comcast.com>
2025-05-31 11:56:23 -0700

    update path in getstats.sh

--- getstats.sh
+++ getstats.sh
@@ -1,10 +1,10 @@
 FN=`ls poker_now_log_*.csv`
 
-/space/poker/analyse-poker.py $FN
+../..//analyse-poker.py $FN
 echo
 
 echo "Bottom 20% aka Garbage List"
-/space/poker/gc.py $FN 120 | sort
+../..//gc.py $FN 120 | sort
 #../gc.py $FN | egrep -v "SB|BB" | sort
 echo
 
